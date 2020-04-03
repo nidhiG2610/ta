8b494bd35f45caa72de15497d965c2e00be710ec
Nidhi2610 <nidhigajera.mobio@gmail.com>
2020-04-03 00:32:51 -0400

    added js nd how-it-works?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,28 @@
 // JavaScript source code
 console.log("hi");
+
+var verticalTimelines = document.getElementsByClassName("timeline"),
+		verticalTimelinesArray = [],
+		scrolling = false;
+	if( verticalTimelines.length > 0 ) {
+		for( var i = 0; i < verticalTimelines.length; i++) {
+			(function(i){
+				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
+			})(i);
+		}
+
+		//show timeline blocks on scrolling
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
+			}
+		});
+	}
+
+	function checkTimelineScroll() {
+		verticalTimelinesArray.forEach(function(timeline){
+			timeline.showBlocks();
+		});
+		scrolling = false;
+	};
